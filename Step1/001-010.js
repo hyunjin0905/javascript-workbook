c9ef5d1f2fcede18007ae955a13c94c43d16efbd
조현진 <kkadangj@gmail.com>
2021-02-05 23:52:35 +0900

    [ADD] Step1/ 009

--- Step1/001-010.js
+++ Step1/001-010.js
@@ -109,3 +109,20 @@ for(i in d) {
     console.log(i)
     console.log(d[i]);
 }
+// #9
+
+
+// var year = '2019';
+// var month = '04';
+// var day = '26';
+// var hour = '11';
+// var minute = '34';
+// var second = '27';
+//
+// var result = //빈칸을 채워주세요
+//
+//     console.log(result);
+//
+//
+// 출력
+// 2019/04/26 11:34:2
