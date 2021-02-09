4b6ba40cd3bde68fbb410069c6a645d410664fd6
조현진 <kkadangj@gmail.com>
2021-02-09 23:05:12 +0900

    [ADD]

--- Step1/001-010.js
+++ Step1/001-010.js
@@ -112,17 +112,32 @@ for(i in d) {
 // #9
 
 
-// var year = '2019';
-// var month = '04';
-// var day = '26';
-// var hour = '11';
-// var minute = '34';
-// var second = '27';
-//
-// var result = //빈칸을 채워주세요
-//
-//     console.log(result);
-//
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+
+var result = year.concat('/', month, '/', day ," ",  hour ,':' , minute ,':', second);
+console.log(result);
+
 //
 // 출력
 // 2019/04/26 11:34:2
+
+// #10
+// 별찍기
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+// 1부터 100까지 모두 더하는 Code를 <pass> 부분에 완성하세요. for를 사용해야 합니다
+
+
+
+
